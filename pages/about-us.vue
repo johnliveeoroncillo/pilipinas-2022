1ed42fd98298c795d10b0fa4a4b11d74c57d60ae
JL <67359294+johnliveeoroncillo@users.noreply.github.com>
2022-06-27 20:45:25 +0800

    Added sample about-us

---
+++ pages/about-us.vue
@@ -0,0 +1,27 @@
+<template>
+    <main>
+        <h1>About Us</h1>
+        <input type="text" v-model="form.username" />
+        Value: {{form.username}}
+
+        <button @click="save">Save</button>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                username: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        async save() {
+            await this.$axios.post('/login', this.form)
+        }
+    }
+}
+</script>
